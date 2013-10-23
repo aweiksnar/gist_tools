2b0c730ff0c04d23d76265f4ba6b053168038251
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-23 12:47:39 -0500

    install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ GistTools::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #TODO: Add corresponding line to production setup when deploying
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
