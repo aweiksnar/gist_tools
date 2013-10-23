7e584baed80c12b2cfd127f8dfe6f2ecc5ce04b2
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-23 09:56:55 -0500

    primary commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GistTools::Application.config.secret_key_base = 'a7badf92808e74d90f5e7341781e745df9cb0d63c4b3becd7a6f8bf6280efc108da2492f7908c17fa2cb8c1a64b4340729b9aae8ea8555f7fded9eb710bd96a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GistTools::Application.config.session_store :cookie_store, key: '_gist_tools_session'
