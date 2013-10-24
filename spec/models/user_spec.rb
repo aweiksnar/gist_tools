fc224b4f43b8a68343236d3a875417d39e9339c8
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-24 11:18:34 -0500

    add a couple User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should exist when created" do
+    u = User.new(email: "test@example.com", password: "test_pass")
+    expect(u.save).to eq(true)
+    expect(u.id.class).to eq(Fixnum)
+    u.destroy
+  end
+
+  it "should require an email and password to sign up" do
+    u = User.new(email: "test2@example.com")
+    expect(u.save).to eq(false)
+    u = User.new(password: "test_pass")
+    expect(u.save).to eq(false)
+  end
 end
