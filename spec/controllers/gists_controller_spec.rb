cb6994493b1bedb79d1f55089fd120da93047e64
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-26 16:57:32 -0500

    add current_user stub to gists controller spec

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe GistsController do
+  before(:each) do
+    controller.stub(:current_user).and_return(User.create(email: "test@example.com", password: "password", github_username: "octocat"))
+  end
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
