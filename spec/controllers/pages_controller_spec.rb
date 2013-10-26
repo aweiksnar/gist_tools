9b06b503222632e688c1911d34c0e4f0b04fe3cc
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-26 18:06:19 -0500

    add some tests for #raw_gists

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,11 @@ describe PagesController do
       get 'index'
       response.should be_success
     end
+
+    it "renders the index template" do
+      get :index
+      response.should render_template("index")
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,19 @@ describe User do
     u = User.new(password: "test_pass")
     expect(u.save).to eq(false)
   end
+
+  describe "#raw_gists" do
+    user = User.new
+    it{expect(user).to respond_to(:raw_gists)}
+
+    it "should return JSON data" do
+      user = User.create(email: "test@example.com", password: "password", github_username: "octocat")
+      expect(user.raw_gists.class).to eq(Array)
+
+      data = user.raw_gists.map{|d| d }
+      expect(data.first.class).to eq(Hash)
+
+      expect(data.first["id"]).to eq("1305321")
+    end
+  end
 end
