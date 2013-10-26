a8ee153dde4ccc73d04d25a163636dd357217f00
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-26 17:45:55 -0500

    add gists controller tests

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -10,5 +10,22 @@ describe GistsController do
       get 'index'
       response.should be_success
     end
+
+    it "renders the index template" do
+      get :index
+      response.should render_template("index")
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', id: "7171195"
+      response.should be_success
+    end
+
+    it "renders the show template" do
+      get 'show', id: "7171195"
+      response.should render_template("show")
+    end
   end
 end
