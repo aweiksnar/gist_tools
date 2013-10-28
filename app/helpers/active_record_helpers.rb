44fc59ce7d002f914bb7ef375413e877f4ed667c
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-28 13:42:23 -0500

    add github access token to users

--- app/helpers/active_record_helpers.rb
+++ app/helpers/active_record_helpers.rb
@@ -8,7 +8,7 @@ module Authentication
     module ClassMethods
       def find_for_oauth(auth)
         record = where(provider: auth.provider, uid: auth.uid.to_s).first
-        record || create(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0,20], name: auth.info.name, github_username: auth.info.nickname)
+        record || create(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0,20], name: auth.info.name, github_username: auth.info.nickname, github_access_token: auth.credentials.token)
       end
     end
   end
